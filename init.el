78b5ce9d2b9a3fda66dff45fbaf37bc8f1d4768c
Mostafa Razavi <mostafa@sepent.com>
2023-02-21 09:07:36 +0100

    Re-enable ido and disable vertico

--- init.el
+++ init.el
@@ -23,9 +23,9 @@
        :completion
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
+       ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)           ; the search engine of the future
+       ;; (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
