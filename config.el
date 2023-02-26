bae24c5a620a24d0c6144a23248d5336264b6bdb
Mostafa Razavi <mostafa@sepent.com>
2023-02-26 03:24:28 +0100

    Enable sly flex style completion

--- config.el
+++ config.el
@@ -75,6 +75,9 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;; enable sly flex style completion
+(setq sly-complete-symbol-function 'sly-flex-completions)
+
 (server-start)
 
 (add-to-list 'exec-path "/home/mostafa/bin")
